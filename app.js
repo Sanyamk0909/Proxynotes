ee7883c737a0343c8e47d65e2b05ca869d16b11d
Sanyam Khosla <sanyamgk@iitk.ac.in>
2020-07-07 19:48:41 +0530

    second

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+const url = 'mongodb://localhost:27017/proxyNotes';
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const mongoose = require('mongoose');
+const connect = mongoose.connect(url);
+connect.then((db) => {
+  console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 var app = express();
 
 // view engine setup
@@ -17,16 +24,44 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//app.use(cookieParser('12345-67890-09876-54321'));
+function auth (req, res, next) {
+  console.log(req.session);
+
+if(!req.session.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+}
+else {
+  if (req.session.user === 'authenticated') {
+    next();
+  }
+  else {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+  }
+}
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
